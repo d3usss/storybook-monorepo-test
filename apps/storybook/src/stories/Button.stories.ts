0f4e8a57bb899e056aff23024b06f26a443ce692
Łukasz Migut <real.d3us@gmail.com>
2025-07-07 19:08:16 +0200

    Change booton

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -14,7 +14,28 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
+  argTypes: {
+    children: {
+      description: "The content of the button.",
+    },
+    className: {
+      control: {
+        type: "text",
+      },
+      type: "string",
+      description: "The CSS class to apply to the button.",
+    },
+    onClick: {
+      action: "clicked",
+      description: "Function to call when the button is clicked.",
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+      description: "If true, the button will be disabled.",
+    },
+  },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
@@ -24,5 +45,16 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {},
+  args: {
+    children: "Click Me",
+    className: "bg-blue-600 text-white",
+  },
+};
+
+export const Disable: Story = {
+  args: {
+    children: "Click Me Disabled",
+    className: "text-black",
+    disabled: true,
+  },
 };
