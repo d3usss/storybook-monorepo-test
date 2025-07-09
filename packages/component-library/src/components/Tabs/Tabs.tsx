f7cc255beb8aea932c6d502f7f5985e909bcd75b
Łukasz Migut <real.d3us@gmail.com>
2025-07-09 18:51:33 +0200

    Remove no neede console.log in Tabs

--- packages/component-library/src/components/Tabs/Tabs.tsx
+++ packages/component-library/src/components/Tabs/Tabs.tsx
@@ -2,8 +2,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function TabsComponent() {
-  console.log("TEST!!!");
-
   return (
     <Tabs defaultValue="account" className="w-[400px]">
       <TabsList>
