96d05fa7ed43924d18de69b134b3a821708127de
Łukasz Migut <real.d3us@gmail.com>
2025-07-27 18:12:42 +0200

    Branch B

--- packages/component-library/src/components/Button/Button.tsx
+++ packages/component-library/src/components/Button/Button.tsx
@@ -1,18 +1,33 @@
 "use client";
 
-import { Button as MuiButton } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
-export default function Button(
-  props: React.ButtonHTMLAttributes<HTMLButtonElement>
-) {
-  const { className, ...restProps } = props;
+interface ButtonComponentProps {
+  className?: string;
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+}
+
+export default function ButtonComponent({
+  className,
+  children,
+  type,
+  onClick,
+}: ButtonComponentProps) {
+  const handleOnClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <MuiButton
-      type="button"
+    <Button
       className={`${className || ""} bg-blue-600`}
-      {...restProps}
+      type={type || "button"}
+      onClick={handleOnClick}
     >
-      Button
-    </MuiButton>
+      {children}
+    </Button>
   );
 }
