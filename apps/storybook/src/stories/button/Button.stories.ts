2755d09545f5eb287f74fcd71e97ad3fd6b584fd
Łukasz Migut <real.d3us@gmail.com>
2025-07-27 18:23:25 +0200

    Some update

--- apps/storybook/src/stories/button/Button.stories.ts
+++ apps/storybook/src/stories/button/Button.stories.ts
@@ -14,7 +14,24 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
+  argTypes: {
+    className: {
+      control: "text",
+      description: "Custom CSS class for the button",
+    },
+    type: {
+      control: "select",
+      options: ["button", "submit", "reset"],
+      description: "Type of the button",
+    },
+    onClick: {
+      action: "clicked",
+      description: "Function to call when the button is clicked",
+    },
+    children: {
+      description: "Content inside the button",
+    },
+  },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
@@ -24,5 +41,9 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {},
+  args: {
+    children: "Click Me",
+    className: "custom-class",
+    type: "button",
+  },
 };
--- packages/component-library/src/main.ts
+++ packages/component-library/src/main.ts
@@ -1,4 +1,3 @@
 import "./styles/global.css";
 
 export { default as Button } from "./components/Button/Button";
-export { default as Pagination } from "./components/Pagination/Pagination";
